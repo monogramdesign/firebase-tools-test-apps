74e9bbf6813cd7c93dd7a3fd8bd0e8981ba75493
Leonardo Ortiz <leo@monogram.io>
2026-08-11 17:57:08 -0300

    apphosting astro adapter test apps

--- astro/apphosting-adapter/v5/src/pages/api/hello.ts
+++ astro/apphosting-adapter/v5/src/pages/api/hello.ts
@@ -1,4 +1,4 @@
 import type { APIRoute } from 'astro';
 export const GET: APIRoute = async () => {
-  return new Response(JSON.stringify({ message: 'Hello from Astro v5 via @apphosting/astro-adapter' }));
+  return new Response(JSON.stringify({ message: 'Hello from Astro v5 via @apphosting/astro-adapter', time: new Date().toISOString() }));
 };
--- astro/apphosting-adapter/v6/src/pages/api/hello.ts
+++ astro/apphosting-adapter/v6/src/pages/api/hello.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async () => {
-  return new Response(JSON.stringify({ message: 'Hello from Astro v6 via @apphosting/astro-adapter' }));
+  return new Response(JSON.stringify({ message: 'Hello from Astro v6 via @apphosting/astro-adapter', time: new Date().toISOString() }));
 };
